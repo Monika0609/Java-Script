cb5b1001fcc249ac2cbd3893aa0057f7b3958063
Monika0609 <63877740+Monika0609@users.noreply.github.com>
2021-05-23 10:18:31 +0530

    Create Greatest_number.js

---
+++ Greatest_number.js
@@ -0,0 +1,13 @@
+var n1=  prompt("enter first numbers");
+var n2 = prompt("enter second number");
+var n3 = prompt("enter third number");
+
+if(n1>=n2 && n1>=n3){
+  console.log("n1 is greater");
+}
+else if ( n2>n1&&n2>n3){
+  console.log("n2 is greater");
+}
+else {
+  console.log("n3 is greater");
+}
